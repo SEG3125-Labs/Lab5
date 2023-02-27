b9c6f5454a153693bc50bb6cef393d5d9187d49b
adrianiannantuono <aiannantuono@me.com>
2023-02-26 20:46:19 -0500

    phone verification, credit verification and date picker disabled dates

--- js/verification.js
+++ js/verification.js
@@ -1,3 +1,21 @@
+var unavailableDates_dave = ["06/29/2020","07/07/2020","07/10/2020"];
+var unavailableDates_jack = ["02/22/2023","02/07/2023","02/10/2023"];
+const setDateFormat = "mm/dd/yy";
+
+function disableDates(date, person) {
+  if (person == 'dave') {
+    // Sunday is Day 0, disable all Sundays
+    if (date.getDay() == 0)
+        return [false];
+    var string = jQuery.datepicker.formatDate(setDateFormat, date);
+    return [ unavailableDates_dave.indexOf(string) == -1 ]
+  } else if (person == 'jack') {
+    console.log("jack")
+    var string = jQuery.datepicker.formatDate(setDateFormat, date);
+    return [ unavailableDates_jack.indexOf(string) == -1 ];
+  }
+}
+
 document.getElementById("toastbtn").onclick = function() {
     var toastElList = [].slice.call(document.querySelectorAll('.toast'))
     var toastList = toastElList.map(function(toastEl) {
@@ -10,70 +28,63 @@ document.getElementById("toastbtn").onclick = function() {
   };
 
   $(document).ready(function($) {
-  $('[id=bookService]').click(function(){
+    $ ('[id=bookService]').click(function(){
+      $('#exampleModalCenter').modal('show');
+      return true;
+    });
 
-    $('#exampleModalCenter').modal('show');
-    return true;
+    $("#exampleInputPhone").on("change", function(){
+      if (!validatePhone("exampleInputPhone")){
+          alert("Format of phone number should be xxx-xxx-xxxx");
+          $("#exampleInputPhone").addClass("error");
+      }
+      else {
+          $("#exampleInputPhone").removeClass("error");
+      }
+    });
+
+    $("#card-number").on("change", function(){
+      if (!validateCreditCard("card-number")){
+          alert("Format of credit card number should be xxxx-xxxx-xxxx-xxxx");
+          $("#card-number").addClass("error");
+      }
+      else {
+          $("#card-number").removeClass("error");
+      }
+    });
+
+    $("input[type=radio][name=expert]").on("change", function(){
+      var person = this.value;
+      $( "#datepicker1" ).datepicker(
+        {
+            dateFormat: setDateFormat,
+            // no calendar before June 1rst 2020
+            minDate: new Date('06/01/2020'),  
+            maxDate: '+4M',
+            // used to disable some dates
+            beforeShowDay: function(date) {
+              return disableDates(date, person);
+            }
+        }   
+      );
+    });
+
+
   });
 
-  $( "#datepicker1" ).datepicker();
-        $( "#datepicker2" ).datepicker();
-        $( "#datepicker3" ).datepicker();
-        $( "#datepicker4" ).datepicker();
-});
+  function validatePhone(txtPhone) {
+    var a = document.getElementById(txtPhone).value;
+    var filter = /^\d{3}[-]\d{3}[-]\d{4}$/;
+    if (filter.test(a)) { return true; }
+    return false;
+  }
 
-  /*$('#submit1').click(function(){
-    if($('#date1').val()==''){
-        alert('Please enter a date for your service.');
-        return false;
-      }
-    if($('#time1').val() == ''){
-        alert('Please enter a time for your service');
-        return false;
-    }
+  function validateCreditCard(creditCard) {
+    var a = document.getElementById(creditCard).value;
+    var filter = /^\d{4}[-]\d{4}[-]\d{4}[-]\d{4}$/;
+    console.log(a)
+    if (filter.test(a)) { return true; }
+    return false;
+  }
 
-    $('#exampleModalCenter').modal('show');
-    return true;
-  });
-
-  $('#submit2').click(function(){
-    if($('#date2').val()==''){
-        alert('Please enter a date for your service.');
-        return false;
-      }
-    if($('#time2').val() == ''){
-        alert('Please enter a time for your service');
-        return false;
-    }
-
-    $('#exampleModalCenter').modal('show');
-    return true;
-  });
-
-  $('#submit3').click(function(){
-    if($('#date3').val()==''){
-        alert('Please enter a date for your service.');
-        return false;
-      }
-    if($('#time3').val() == ''){
-        alert('Please enter a time for your service');
-        return false;
-    }
-
-    $('#exampleModalCenter').modal('show');
-    return true;
-  });
-
-  $('#submit4').click(function(){
-    if($('#date4').val()==''){
-        alert('Please enter a date for your service.');
-        return false;
-      }
-    if($('#time4').val() == ''){
-        alert('Please enter a time for your service');
-        return false;
-    }
-
-    $('#exampleModalCenter').modal('show');
-    return true;
-  });*/
+ 
